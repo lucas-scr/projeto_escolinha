79824e882716031e25efcdde511d96fa2ba0f95d
Lucas de Souza Carvalho <lucas.carvalhu.ferreira@gmail.com>
2025-02-09 20:27:54 -0300

    Add component de cadastro de alunos

---
+++ src/app/components/cadastro-alunos/cadastro-alunos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-alunos',
+  imports: [],
+  templateUrl: './cadastro-alunos.component.html',
+  styleUrl: './cadastro-alunos.component.css'
+})
+export class CadastroAlunosComponent {
+
+}
