3137a7623032def40550b68aa78c8b6c460f197c
Lucas de Souza Carvalho <lucas.carvalhu.ferreira@gmail.com>
2025-02-14 19:40:06 -0300

    Adicionado service e model dos contratos

--- src/app/model/Contrato.ts
+++ src/app/model/Contrato.ts
@@ -1,20 +1,110 @@
-import { Aluno } from "./Alunos";
-import { Responsavel } from "./Responsavel";
+import { Aluno } from './Alunos';
+import { Responsavel } from './Responsavel';
 
-export class Contrato{
+export class Contrato {
+  private _id: Number;  
+  private _responsavel: Responsavel;
+  private _aluno: Aluno;
+  private _dataCadastro: Date;
+  private _dataInicio: Date;
+  private _dataFim?: Date;
+  private _dias: String[];
+  private _valor: Number;
+  private _dataPagamento: Date;
 
-    private responsavel: Responsavel;
-    private aluno: Aluno;
-    private dataCadastro: Date;
-    private dataInicio: Date;
-    private dataFim?: Date;
-    private dias: [5];
-    private valor: Number;
-    private dataPagamento: Date;
-    
-    constructor(){
+  constructor(
+    responsavel: Responsavel,
+    aluno: Aluno,
+    dataInicio: Date,
+    dias: String[],
+    valor: Number,
+    dataPagamento: Date,
+    dataFim?: Date
+  ) {
+    this._responsavel = responsavel,
+    this._aluno = aluno,
+    this._dataCadastro = new Date();
+    this._dataInicio = dataInicio;
+    this._dias = dias;
+    this._valor = valor;
+    this._dataPagamento = dataPagamento;
+  }
 
+  // Getters
+  get responsavel(): Responsavel {
+    return this._responsavel;
+  }
+
+  get aluno(): Aluno {
+    return this._aluno;
+  }
+
+  get dataCadastro(): Date {
+    return this._dataCadastro;
+  }
+
+  get dataInicio(): Date {
+    return this._dataInicio;
+  }
+
+  get dataFim(): Date | undefined {
+    return this._dataFim;
+  }
+
+  get dias(): String[] {
+    return this._dias;
+  }
+
+  get valor(): Number {
+    return this._valor;
+  }
+
+  get dataPagamento(): Date {
+    return this._dataPagamento;
+  }
+  get id(): Number {
+    return this._id;
+  }
+
+
+  // Setters
+  set responsavel(responsavel: Responsavel) {
+    this._responsavel = responsavel;
+  }
+
+  set aluno(aluno: Aluno) {
+    this._aluno = aluno;
+  }
+
+  set dataCadastro(dataCadastro: Date) {
+    this._dataCadastro = dataCadastro;
+  }
+
+  set dataInicio(dataInicio: Date) {
+    this._dataInicio = dataInicio;
+  }
+
+  set dataFim(dataFim: Date | undefined) {
+    this._dataFim = dataFim;
+  }
+
+  set dias(dias: String[]) {
+    if (dias.length > 5) {
+      throw new Error('O número máximo de dias permitidos é 5.');
     }
+    this._dias = dias;
+  }
+
+  set valor(valor: Number) {
+    this._valor = valor;
+  }
+
+  set dataPagamento(dataPagamento: Date) {
+    this._dataPagamento = dataPagamento;
+  }
 
 
+  set id(id: Number) {
+    this._id = id;
+  }
 }
--- src/app/services/service_contratos.ts
+++ src/app/services/service_contratos.ts
@@ -1,14 +1,153 @@
-import { Injectable } from "@angular/core";
-import { Aluno } from "../model/Alunos";
-import { Responsavel } from "../model/Responsavel";
+import { Injectable } from '@angular/core';
+import { Contrato } from '../model/Contrato';
+import { Aluno } from '../model/Alunos';
+import { Responsavel } from '../model/Responsavel';
 
 @Injectable({
-  providedIn: 'root' // Torna o serviço disponível globalmente
+  providedIn: 'root', // Torna o serviço disponível globalmente
 })
+export class ServiceContratos {
+  private listaContratos: Contrato[] = [];
 
+  constructor() {
+    let aluno1: Aluno = new Aluno('Carlim', new Date('2000-05-02'), false, 'M');
+    let aluno2: Aluno = new Aluno('Joao', new Date('2001-08-04'), true, 'M');
+    let aluno3: Aluno = new Aluno('Maria', new Date('1999-10-02'), false, 'F');
+    let aluno4: Aluno = new Aluno('Aninha', new Date('1999-10-28'), false, 'F');
+    let aluno5: Aluno = new Aluno(
+      'Lucas Silva',
+      new Date('2014-05-12'),
+      true,
+      'M'
+    );
+    let aluno6: Aluno = new Aluno(
+      'Mariana Souza',
+      new Date('2016-09-23'),
+      false,
+      'F'
+    );
 
-export class ServiceContratos{
-    private aluno: Aluno;
-    private responsavel: Responsavel
+    let responsavel1: Responsavel = new Responsavel(
+      'Responsável 1',
+      '12345678901'
+    );
+    let responsavel2: Responsavel = new Responsavel(
+      'Responsável 2',
+      '12345678902'
+    );
+    let responsavel3: Responsavel = new Responsavel(
+      'Responsável 3',
+      '12345678903'
+    );
+    let responsavel4: Responsavel = new Responsavel(
+      'Responsável 4',
+      '12345678904'
+    );
+    let responsavel5: Responsavel = new Responsavel(
+      'Responsável 5',
+      '12345678905'
+    );
+    let responsavel6: Responsavel = new Responsavel(
+      'Responsável 6',
+      '12345678906'
+    );
 
+    this.listaContratos = [
+      new Contrato(
+        responsavel1,
+        aluno1,
+        new Date('2024-03-01'),
+        ['Segunda'],
+        500,
+        new Date('2024-02-28')
+      ),
+      new Contrato(
+        responsavel2,
+        aluno2,
+        new Date('2024-04-01'),
+        ['Terça', 'Quinta'],
+        400,
+        new Date('2024-03-31')
+      ),
+      new Contrato(
+        responsavel3,
+        aluno3,
+        new Date('2024-05-01'),
+        ['Segunda', 'Quarta', 'Sexta'],
+        600,
+        new Date('2024-04-30'),
+        new Date('2024-12-31')
+      ),
+      new Contrato(
+        responsavel4,
+        aluno4,
+        new Date('2024-06-01'),
+        ['Terça', 'Quinta'],
+        450,
+        new Date('2024-05-31')
+      ),
+      new Contrato(
+        responsavel5,
+        aluno5,
+        new Date('2024-07-01'),
+        ['Segunda', 'Quarta'],
+        550,
+        new Date('2024-06-30'),
+        new Date('2024-11-30')
+      ),
+      new Contrato(
+        responsavel6,
+        aluno6,
+        new Date('2024-08-01'),
+        ['Segunda', 'Quarta', 'Sexta'],
+        500,
+        new Date('2024-07-31')
+      ),
+    ];
+
+    this.listaContratos[0].id = 1;
+    this.listaContratos[1].id = 2;
+    this.listaContratos[2].id = 3;
+    this.listaContratos[3].id = 4;
+    this.listaContratos[4].id = 5;
+    this.listaContratos[5].id = 6;
+  }
+
+  adicionarContratoNalista(novoContrato: Contrato) {
+    this.listaContratos.push(novoContrato);
+  }
+
+  removerContratoDaLista(id: Number) {
+    const index = this.listaContratos.findIndex(
+      (alunoLista) => alunoLista.id === id
+    );
+
+    if (index !== -1) {
+      this.listaContratos.splice(index, 1);
+    }
+  }
+
+  listarContrato(): Contrato[] {
+    return this.listaContratos;
+  }
+
+  capturarUltimoId(): Number {
+    const ultimoId = this.listaContratos.length + 1;
+    return ultimoId;
+  }
+
+  findBydId(id: Number): Contrato {
+    const contratoEncontrado = this.listaContratos.find((item) => item.id == id);
+    return contratoEncontrado;
+  }
+
+  AtualizarContratoNalista(contrato: Contrato){
+    this.listaContratos = this.listaContratos.map(item => {
+        if(item.id = contrato.id){
+          return  item = contrato;
+        } else{
+            return item;
+        }
+     } );
+}
 }
